a454a2f3f7c1667b8e6b793dcc112cde31b6e98e
Michael da Costa Móra <michael.c.mora@gmail.com>
2019-06-19 11:03:28 -0300

    Acrescentando implemantação da classe Knapsack.
    Com método recursivo do problema da mochila.

---
+++ src/test/java/br/pucrs/progdinam/KnapsackTest.java
@@ -0,0 +1,17 @@
+package br.pucrs.progdinam;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KnapsackTest {
+
+	@Test
+	void KnapSackRecursiveTest1() {
+		long [] pesos = {12, 4, 2, 1, 1};
+		long [] valor = {4, 10, 2, 2, 1};
+		long res = Knapsack.KnapSackRecursive(15, pesos, valor);
+		
+		assertEquals(14, res);
+	}
+}
