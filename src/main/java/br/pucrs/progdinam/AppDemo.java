30e046668a0c2b5b82f8456e4b12b1631b878545
Michael da Costa Móra <michael.c.mora@gmail.com>
2020-09-23 21:44:51 -0300

    Ajustes no demo.

--- src/main/java/br/pucrs/progdinam/AppDemo.java
+++ src/main/java/br/pucrs/progdinam/AppDemo.java
@@ -10,48 +10,48 @@ public class AppDemo {
 		long [] pesos;
 		long [] valor;
 	 
-//		System.out.println("Edit Distance\n");
-//		str1 = "O dia da semana";
-//		str2 = "quinta ou";
-//		System.out.println("Textos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
-//		res = ed.EDProgDinam(str1, str2);
-//		System.out.println("Distancia (Prog Dinam) = " + res);
-//		res = ed.EDRecursive(str1, str2);
-//		System.out.println("Distancia (Recursive) " + res);
-//
-//		System.out.println("Edit Distance\n");
-//		str1 = "abcdefghijklmnop";
-//		str2 = "rstuvwxyz";
-//		System.out.println("Textos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
-//		res = ed.EDProgDinam(str1, str2);
-//		System.out.println("Distancia (Prog Dinam) = " + res);
-//		res = ed.EDRecursive(str1, str2);
-//		System.out.println("Distancia (Recursive) " + res);
-//
-//		
-//		str1 = "Maven, a Yiddish word meaning";
-//		str2 = "This post is not about deep";
-//		System.out.println("\nTextos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
-//		res = ed.EDProgDinam(str1, str2);
-//		System.out.println("Distancia (Prog Dinam) = " + res);
-//
-//		str1 = "Maven, a Yiddish word meaning accumulator of knowledge, began as an attempt to " +
-//				"simplify the build processes in the Jakarta Turbine project. There were several" + 
-//				" projects, each with their own Ant build files, that were all slightly different." +
-//				"JARs were checked into CVS. We wanted a standard way to build the projects, a clear "+ 
-//				"definition of what the project consisted of, an easy way to publish project information" +
-//				"and a way to share JARs across several projects. The result is a tool that can now be" +
-//				"used for building and managing any Java-based project. We hope that we have created " +
-//				"something that will make the day-to-day work of Java developers easier and generally help " +
-//				"with the comprehension of any Java-based project.";
-//		str2 = "This post is not about deep learning. But it could be might as well. This is the power of " +
-//				"kernels. They are universally applicable in any machine learning algorithm. Why you might" +
-//				"ask? I am going to try to answer this question in this article.\r\n" + 
-//			   "Go to the profile of Marin Vlastelica Pogančić" + 
-//			   "Marin Vlastelica Pogančić Jun";
-//		System.out.println("\nTextos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
-//		res = ed.EDProgDinam(str1, str2);
-//		System.out.println("Distancia (Prog Dinam) = " + res);
+		System.out.println("Edit Distance\n");
+		str1 = "Casablanca";
+		str2 = "Portentoso";
+		System.out.println("Textos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
+		res = ed.EDProgDinam(str1, str2);
+		System.out.println("Distancia (Prog Dinam) = " + res);
+		res = ed.EDRecursive(str1, str2);
+		System.out.println("Distancia (Recursive) " + res);
+
+		System.out.println("Edit Distance\n");
+		str1 = "abcdefghijklmnop";
+		str2 = "rstuvwxyz";
+		System.out.println("Textos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
+		res = ed.EDProgDinam(str1, str2);
+		System.out.println("Distancia (Prog Dinam) = " + res);
+		res = ed.EDRecursive(str1, str2);
+		System.out.println("Distancia (Recursive) " + res);
+
+		
+		str1 = "Maven, a Yiddish word meaning";
+		str2 = "This post is not about deep";
+		System.out.println("\nTextos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
+		res = ed.EDProgDinam(str1, str2);
+		System.out.println("Distancia (Prog Dinam) = " + res);
+
+		str1 = "Maven, a Yiddish word meaning accumulator of knowledge, began as an attempt to " +
+				"simplify the build processes in the Jakarta Turbine project. There were several" + 
+				" projects, each with their own Ant build files, that were all slightly different." +
+				"JARs were checked into CVS. We wanted a standard way to build the projects, a clear "+ 
+				"definition of what the project consisted of, an easy way to publish project information" +
+				"and a way to share JARs across several projects. The result is a tool that can now be" +
+				"used for building and managing any Java-based project. We hope that we have created " +
+				"something that will make the day-to-day work of Java developers easier and generally help " +
+				"with the comprehension of any Java-based project.";
+		str2 = "This post is not about deep learning. But it could be might as well. This is the power of " +
+				"kernels. They are universally applicable in any machine learning algorithm. Why you might" +
+				"ask? I am going to try to answer this question in this article.\r\n" + 
+			   "Go to the profile of Marin Vlastelica Pogančić" + 
+			   "Marin Vlastelica Pogančić Jun";
+		System.out.println("\nTextos: \n" + "String 1: " + str1 + "\nString 2: " + str2);
+		res = ed.EDProgDinam(str1, str2);
+		System.out.println("Distancia (Prog Dinam) = " + res);
 		
 		System.out.println("\n\nKnapsack\n");
 		pieces = new KnapSack.KnapSackPiece[10];
@@ -73,5 +73,22 @@ public class AppDemo {
 		System.out.println("");
 		res = KnapSack.knapSackRecursive(165, pieces);
 		System.out.println("Maior valor (Knapsack Recursivo) = " + res );
+		
+		pieces = new KnapSack.KnapSackPiece[6];
+		pieces[0] = new KnapSack.KnapSackPiece(56, 50);
+		pieces[1] = new KnapSack.KnapSackPiece(59, 50);
+		pieces[2] = new KnapSack.KnapSackPiece(80, 64);
+		pieces[3] = new KnapSack.KnapSackPiece(64, 46);
+		pieces[4] = new KnapSack.KnapSackPiece(75, 50);
+		pieces[5] = new KnapSack.KnapSackPiece(17, 05);
+
+		System.out.println("\nPares: (peso, valor)");
+		for (int i = 0; i < pieces.length; i ++) {
+			System.out.print("(" + pieces[i].getPeso() + ", " + pieces[i].getValor() + ")");
+		}
+		System.out.println("");
+		res = KnapSack.knapSackRecursive(190, pieces);
+		System.out.println("Maior valor (Knapsack Recursivo) = " + res );
+
 	}
 }
