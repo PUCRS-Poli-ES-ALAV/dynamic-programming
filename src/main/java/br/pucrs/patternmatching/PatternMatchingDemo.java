84bd2415a41a5eac263a08daaa4d57c6c306e7db
Michael da Costa Móra <michael.c.mora@gmail.com>
2022-02-05 04:27:00 -0300

    Migrando para VSCode

--- src/main/java/br/pucrs/patternmatching/PatternMatchingDemo.java
+++ src/main/java/br/pucrs/patternmatching/PatternMatchingDemo.java
@@ -2,6 +2,8 @@ package br.pucrs.patternmatching;
 import br.pucrs.util.*;
 
 public class PatternMatchingDemo {
+	private static final int NRO_TESTS = 4;
+//	private static final int NRO_TESTS = 1;
 	private static String geraString(int size, int rep) {
 		StringBuilder res = new StringBuilder();;
 		String charStr = "ABCDEFGHIJKLMOPQRSTWXYZ";
@@ -18,24 +20,39 @@ public class PatternMatchingDemo {
 		return res.toString();
 	}
 	
+	public static void printResults(String txt, String pat, boolean show, String [] testsNames, long [] testsRes) {
+		System.out.println("txt = " + txt + "\npat = " + pat);
+		
+		for (int i = 0; i < NRO_TESTS; i++)
+			System.out.println("\t       " + testsNames[i] + " -> " + testsRes[i]);
+	}
+	
 	public static void main(String[] args) {
 		PatternMatching pm = new PatternMatching();
 		TimeCounter timer = new TimeCounter();
 		double segundos;
 		long posicao;
+		long [] testsRes = new long[NRO_TESTS];
+		String [] testsNames = new String[NRO_TESTS];
+
+		testsNames[0] = "Brute Force";
+		testsNames[1] = "Rabin-Karp";
+		testsNames[2] = "Rabin-KarpV2";
+		testsNames[3] = "Knuth-Morris-Prat";
+		
 		
 		System.out.println("*******************************");
-		System.out.println("*Pequenos testes de verifica��o");
+		System.out.println("*Pequenos testes de verifição");
 		System.out.println("*******************************");
 		
 		String txt = "ABCDCBDCBDACBDABDCBADF"; 
 		String pat = "ADF";
-		System.out.println("txt = " + txt + "\npat = " + pat);
-		System.out.println("\t       Brute Force -> " + pm.pmBruteForce(txt, pat));
-		System.out.println("\t       Rabin-Karp -> " + pm.pmRabinKarp(txt, pat));
-		System.out.println("\t       Rabin-KarpV2 -> " + pm.pmRabinKarpV2(txt, pat));
-		System.out.println("\tKnuth-Morris-Prat -> " + pm.pmKnuthMorrisPratt(txt, pat));
-		
+		testsRes[0] = pm.pmBruteForce(txt, pat);
+		testsRes[1] = pm.pmRabinKarp(txt, pat);
+		testsRes[2] = pm.pmRabinKarpV2(txt, pat);
+		testsRes[3] = pm.pmKnuthMorrisPratt(txt, pat);
+		printResults(txt, pat, true, testsNames, testsRes);
+			
 		txt = "ABCDCBDCBDACBDABDCBADF"; 
 		pat = "JJJ";
 		System.out.println("\ntxt = " + txt + "\npat = " + pat);
@@ -45,9 +62,9 @@ public class PatternMatchingDemo {
 		System.out.println("\tKnuth-Morris-Prat -> " + pm.pmKnuthMorrisPratt(txt, pat));
 		
 		System.out.println("\ntxt = " + pat + "\npat = " + txt);
-		System.out.println("\t       Brute Force -> " + pm.pmBruteForce(txt, pat));
+		System.out.println("\t       Brute Force -> " + pm.pmBruteForce(pat, txt));
 		System.out.println("\t       Rabin-Karp -> " + pm.pmRabinKarp(pat, txt));
-		System.out.println("\t       Rabin-KarpV2 -> " + pm.pmRabinKarpV2(txt, pat));
+		System.out.println("\t       Rabin-KarpV2 -> " + pm.pmRabinKarpV2(pat, txt));
 		System.out.println("\tKnuth-Morris-Prat -> " + pm.pmKnuthMorrisPratt(pat, txt));
 		
 		txt = PatternMatchingDemo.geraString(50, 20); 
